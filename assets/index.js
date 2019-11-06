2584118ef7aca8e4f89a7b5f1cba1cc9ee8258c4
Paul Savoie <paul.savoie@rubicon.eu>
2019-11-06 14:33:07 +0100

    adds cookie acknowledge footer

--- assets/index.js
+++ assets/index.js
@@ -188,4 +188,35 @@ document.addEventListener('DOMContentLoaded', function() {
 
     recreateLinks();
   }
+
+  // set cookie acknowledgement
+  // functions taken from https://www.w3schools.com/js/js_cookies.asp
+  function setCookie(cname, cvalue) {
+    document.cookie = cname + "=" + cvalue + ";path=/";
+  }
+
+  function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
+  var cookieConsentCookieName = 'acknowledged-cookies';
+  if (getCookie(cookieConsentCookieName) != 'true') {
+    document.getElementById('cookie-info').classList.add('show');
+    document.getElementById('acknowledge-cookies-btn').addEventListener('click', function() {
+      setCookie(cookieConsentCookieName, 'true');
+      document.getElementById('cookie-info').classList.remove('show');
+    })
+  }
 })
+
