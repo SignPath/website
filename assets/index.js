2bb3287af8d8028da26ce01517be9e6010a038ad
Paul Savoie <paul.savoie@rubicon.eu>
2020-03-31 17:49:50 +0200

    fixes pricing js

--- assets/index.js
+++ assets/index.js
@@ -120,7 +120,7 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   }
 
-  if (window.location.pathname.endsWith('/pricing/')) {
+  if (window.location.pathname.endsWith('/pricing/') || window.location.pathname.endsWith('/pricing')) {
     // calculate prices correctly
     function recalculatePrices() {
       var currency = document.getElementById('currency-toggle').checked ? '€' : '$';
