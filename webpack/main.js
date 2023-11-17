d6c52dffd1e81cadd41ff63be804464680faf069
Paul Savoie <paul.savoie@rubicon.eu>
2023-11-17 12:32:13 +0100

    adds references banner

--- webpack/main.js
+++ webpack/main.js
@@ -1,7 +1,9 @@
 import {cookieBanner} from "./cookie.js";
 import {changelog} from "./changelog.js";
+import {carousel} from "./carousel.js"
 
 document.addEventListener('DOMContentLoaded', function () {
     cookieBanner();
     changelog();
+    carousel();
 });
